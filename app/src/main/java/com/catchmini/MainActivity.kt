73de3aa71245595aa1c8793e57d42692119d54f5
mint <return-1@naver.com>
2024-01-01 23:02:28 +0900

    컴파일SDK 버전 수정 , Toast 함수

--- app/src/main/java/com/catchmini/MainActivity.kt
+++ app/src/main/java/com/catchmini/MainActivity.kt
@@ -2,10 +2,16 @@ package com.catchmini
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        toast("안녕하세요!")
+    }
+
+    fun toast(_str:String){
+        Toast.makeText(baseContext,_str,Toast.LENGTH_SHORT).show()
     }
 }
